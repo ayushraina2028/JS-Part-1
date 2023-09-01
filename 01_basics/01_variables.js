346dca50b5243440550665e99f5289837ab5d7e5
Ayush Raina <ayushraina@iisc.ac.in>
2023-09-01 13:02:27 +0530

    basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accoundId = 134526
+let accountEmail = "ayushraina@iisc.ac.in"
+var accountPassword = "123456"
+accountCity = "Bangalore"  // this is possible but do not prefer this
+
+// const cannot be changed
+
+accountEmail = "ayushraina767@gmail.com"
+accountPassword = "123"
+accountCity = "Jammu"
+
+// Only Declaration
+let accountState;
+
+// To print all at once in 
+console.table([accoundId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Prefer not to use var
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";  // treat all js code as newer version
+
+// alert() , gives popup in browser
+// Code readability is priority
+
+let name = "Ayush Raina"
+let age = 19
+let isLoggedIn = false
+
+// number, bigInt, string, boolean
+// null => standalone value
+// undefined => variable declared but value not given
+// symbol => uniqueness
+// object -> later detail discussion
+
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
