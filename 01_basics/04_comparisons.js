36ca7cd513f56cd3d0d3f7ef6c8e4c33562210ac
Ayush Raina <ayushraina@iisc.ac.in>
2023-09-01 13:29:21 +0530

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,10 @@
+// Basic Comparisons we can do
+
+// Bad things
+console.log("2" > 1);
+console.log("02" > 3)
+console.log(null > 0)
+console.log(null <= 0) 
+
+// === (strick comparison because this checks data type also)
+// == (this makes convrsions in data types)
